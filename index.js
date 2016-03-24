95e9758e70317504592972eeb4e15d3049b093b2
jaimeweed <doblev@yahoo.es>
2016-03-24 19:39:13 +0100

    averaver

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/about",(req,res) => {
 	
 	res.write ("<html><body>Pagina presentacion de Jaime Herrera Y Jose Llopis <ul> ");
 	res.write ("<li>Jaime Herrera Varo </li>");
-	res.write ("<li>Jose Llopis </li></ul></body></html>");
+	res.write ("<li>Jose Llopis </li></ul><a href="finofilipino.org">Enlace</a></body></html>");
 	res.end();
 });
 
