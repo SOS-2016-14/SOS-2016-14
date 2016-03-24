49f82a2164929071241dbad658d0070da9d94a94
jaimeweed <doblev@yahoo.es>
2016-03-24 16:44:50 +0100

    First version

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require ("express");
+var fs = require ("fs");
+var contacts = [];
+var app = express();
+app.get("/", (req,res)=>{
+	fs.readFile('contacts.json','utf8',(err,content)=>{
+		console.log("Data read");
+		contacts = JSON.parse(content);
+		res.write("<html><body>___Contacts___<ul>");
+		contacts.forEach((contact) => {
+			res.write("<li> - "+contact.name+" ("+contact.phone+")</li>");
+
+		});
+		res.write("</ul>______________</body></html>");
+		res.end();
+	});
+});
+
+app.listen(16071);
