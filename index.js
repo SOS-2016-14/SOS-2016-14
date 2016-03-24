d0ba7a93ff73d7875d1625851f8cc73986e2d88d
jaimeweed <doblev@yahoo.es>
2016-03-24 18:43:11 +0100

    enlace añadido

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ var app = express();
 app.get("/about",(req,res) => {
 	console.log("executing");
 
-	res.write ("Página presentación de Jaime Y Jose");
+	res.write (<html><body><center>"Pagina presentacion de Jaime Herrera Y Jose Llopis");
+	res.write (<ul><li>Jaime Herrera Varo</li><a href="https://github.com/jaimeweed">Enlace a github</a></ul></center></body></html>);
 	res.end();
 });
+
 
 app.listen(process.env.PORT);
