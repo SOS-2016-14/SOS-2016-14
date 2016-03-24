98811709a027576aacd4aae3d6f9b843a14c772e
jaimeweed <doblev@yahoo.es>
2016-03-24 19:14:27 +0100

    averaver

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/about",(req,res) => {
 	console.log("executing");
 	
 	res.write ("<html><body>Pagina presentacion de Jaime Herrera Y Jose Llopis <ul> ");
-	res.write ("<li>Jaime Herrera Varo <a href="github.com/jaimeweed"></a></li></ul></body></html>");
+	res.write ("<li>Jaime Herrera Varo </li></ul></body></html>");
 	res.end();
 });
 
