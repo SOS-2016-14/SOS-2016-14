1b6f2c5523022c628b1254c039887bed4fdd4217
jaimeweed <doblev@yahoo.es>
2016-04-05 01:22:10 +0200

    meto localización geográfica

--- index.js
+++ index.js
@@ -101,21 +101,27 @@ app.put("/api/v1/consumissions/:city/:anio",(req,res)=>{
 	var anio = req.params.anio;
 	var contact = req.body;
 	var ok = false;
+	var cont = 0;
+	
 	if(apikey == uuid){	
 		
 
 		contacts.forEach(function(value, key){
 			if(value.year == anio && value.city == city){
+				cont++;
 				contacts[key] = contact;			
 				ok = true
 			}
 
 		});
-
-		if(ok == true)
-			res.sendStatus(200);
-		else
-			res.sendStatus(404);;	
+		if(cont>1){
+			if(ok == true)
+				res.sendStatus(200);
+			else
+				res.sendStatus(404);
+		}else{
+			res.sendStatus(409);
+		}	
 	}else{
 		res.sendStatus(401);
 	}		
